5e41fdcd20b49a7f45306cb77b305d643336acf2
WooYoungDoo <duden5000@naver.com>
2024-01-25 01:21:47 +0900

    Initial commit

---
+++ src/main/java/com/sendback/SendbackApplication.java
@@ -0,0 +1,13 @@
+package com.sendback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sendback/SendbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sendback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
