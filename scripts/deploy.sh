368a5cf6a54e6af40d24dc52320508c744fbb871
WooYoungDoo <duden5000@naver.com>
2024-01-26 18:10:19 +0900

    docs : green blue 위치 변경 #1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,23 +8,24 @@ DOCKER_APP_NAME=spring
 # 실행중인 blue가 있는지
 EXIST_BLUE=$(sudo docker-compose -p DOCKER_APP_NAME-blue -f docker-compose.blue.yml ps | grep Up)
 
-# green이 실행중이면 blue up
-if [ -z "$EXIST_BLUE" ]; then
-	echo "blue up"
-	sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
-
-	sleep 30
-
-	sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
-	sudo docker image prune -af # 사용하지 않는 이미지 삭제
-
 # blue가 실행중이면 green up
-else
+if [ -z "$EXIST_BLUE" ]; then
 	echo "green up"
-	sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+  	sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
 
-	sleep 30
+  	sleep 30
 
-	sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
-	sudo docker image prune -af
+  	sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+  	sudo docker image prune -af
+
+# green이 실행중이면 blue up
+else
+  echo "blue up"
+  	sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+
+  	sleep 30
+
+  	sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+  	sudo docker image prune -af # 사용하지 않는 이미지 삭제
+
 fi
