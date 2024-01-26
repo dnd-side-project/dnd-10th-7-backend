d5d1116468b1d7214cd4b5f61ec588d78b1143de
WooYoungDoo <duden5000@naver.com>
2024-01-26 17:41:24 +0900

    docs : 실행 권한 설정 #1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,25 +6,25 @@ DOCKER_APP_NAME=spring
 
 
 # 실행중인 blue가 있는지
-EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep running)
+EXIST_BLUE=$(sudo docker-compose -p spring-blue -f docker-compose.blue.yml ps | grep running)
 
 # green이 실행중이면 blue up
 if [ -z "$EXIST_BLUE" ]; then
 	echo "blue up"
-	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+	sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
 
 	sleep 30
 
-	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+	sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
 	sudo docker image prune -af # 사용하지 않는 이미지 삭제
 
 # blue가 실행중이면 green up
 else
 	echo "green up"
-	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+	sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
 
 	sleep 30
 
-	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+	sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
 	sudo docker image prune -af
 fi
