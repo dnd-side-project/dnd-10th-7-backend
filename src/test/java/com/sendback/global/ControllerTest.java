768041753373099782d4674cb58b5dc9ae758cec
WooYoungDoo <duden5000@naver.com>
2024-02-13 15:36:12 +0900

    test : restDocs 통합 # 13

--- src/test/java/com/sendback/global/ControllerTest.java
+++ src/test/java/com/sendback/global/ControllerTest.java
@@ -1,19 +1,30 @@
 package com.sendback.global;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sendback.domain.auth.controller.AuthController;
+import com.sendback.domain.auth.service.AuthService;
+import com.sendback.domain.auth.service.GoogleService;
+import com.sendback.domain.auth.service.KakaoService;
+
 import com.sendback.domain.project.controller.ProjectController;
 import com.sendback.domain.project.service.ProjectService;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+
 @WebMvcTest({
-        ProjectController.class
+        ProjectController.class,
+        AuthController.class
 })
 @ActiveProfiles("test")
+@AutoConfigureRestDocs
 public abstract class ControllerTest {
 
     @Autowired
@@ -26,5 +37,12 @@ public abstract class ControllerTest {
     protected ProjectService projectService;
 
     @MockBean
-    protected JpaMetamodelMappingContext jpaMetamodelMappingContext;
+    protected KakaoService kakaoService;
+
+    @MockBean
+    protected GoogleService googleService;
+
+    @MockBean
+    protected AuthService authService;
+
 }
