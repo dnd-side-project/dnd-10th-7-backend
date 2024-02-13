e5dd015465ef67d9b4198f743ed2ff20e29a081c
WooYoungDoo <duden5000@naver.com>
2024-02-13 16:57:57 +0900

    feat : scrap 클릭 api #16

---
+++ src/main/java/com/sendback/domain/scrap/controller/ScrapController.java
@@ -0,0 +1,29 @@
+package com.sendback.domain.scrap.controller;
+
+import com.sendback.domain.scrap.dto.response.ClickScrapResponse;
+import com.sendback.domain.scrap.service.ScrapService;
+import com.sendback.global.common.ApiResponse;
+import com.sendback.global.common.UserId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.sendback.global.common.ApiResponse.success;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/projects")
+public class ScrapController {
+
+    private final ScrapService scrapService;
+
+    @PutMapping("/{projectId}/scrap")
+    public ApiResponse<ClickScrapResponse> reactLike(
+            @UserId Long userId,
+            @PathVariable Long projectId) {
+        return success(scrapService.click(userId, projectId));
+    }
+
+}
