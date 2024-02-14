ad2825ed70bf4899e392601b90cc35645197034c
WooYoungDoo <duden5000@naver.com>
2024-02-14 19:42:58 +0900

    test : feedback api test #21

---
+++ src/test/java/com/sendback/domain/feedback/repository/FeedbackSubmitRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.sendback.domain.feedback.repository;
+
+import com.sendback.domain.feedback.entity.FeedbackSubmit;
+import com.sendback.global.RepositoryTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class FeedbackSubmitRepositoryTest extends RepositoryTest {
+
+    @Autowired
+    FeedbackSubmitRepository feedbackSubmitRepository;
+
+    @Nested
+    @DisplayName("user와 feedback으로 삭제되지 않은 피드백 제출이 있는 지 조회했을 때")
+    class existsByUserAndFeedbackAndIsDeletedIsFalse {
+
+        @Test
+        @DisplayName("값이 있으면 true를 반환한다.")
+        public void success_true() throws Exception {
+            //given
+            FeedbackSubmit feedbackSubmit = feedbackSubmitTestPersister.save();
+
+            //when
+            boolean exists = feedbackSubmitRepository.existsByUserAndFeedbackAndIsDeletedIsFalse(feedbackSubmit.getUser(), feedbackSubmit.getFeedback());
+
+            //then
+            assertThat(exists).isTrue();
+
+        }
+
+        @Test
+        @DisplayName("삭제된 값이 있으면 false를 반환한다.")
+        public void success_false() throws Exception {
+            //given
+            FeedbackSubmit feedbackSubmit = feedbackSubmitTestPersister.save();
+            feedbackSubmitRepository.delete(feedbackSubmit);
+
+            //when
+            boolean exists = feedbackSubmitRepository.existsByUserAndFeedbackAndIsDeletedIsFalse(feedbackSubmit.getUser(), feedbackSubmit.getFeedback());
+
+            //then
+            assertThat(exists).isFalse();
+
+        }
+    }
+
+}
---
+++ src/test/java/com/sendback/domain/feedback/service/FeedbackServiceTest.java
@@ -0,0 +1,207 @@
+package com.sendback.domain.feedback.service;
+
+import com.sendback.domain.feedback.dto.request.SaveFeedbackRequest;
+import com.sendback.domain.feedback.dto.response.FeedbackDetailResponse;
+import com.sendback.domain.feedback.dto.response.FeedbackIdResponse;
+import com.sendback.domain.feedback.dto.response.SubmitFeedbackResponse;
+import com.sendback.domain.feedback.entity.Feedback;
+import com.sendback.domain.feedback.entity.FeedbackSubmit;
+import com.sendback.domain.feedback.repository.FeedbackRepository;
+import com.sendback.domain.feedback.repository.FeedbackSubmitRepository;
+import com.sendback.domain.project.entity.Project;
+import com.sendback.domain.project.service.ProjectService;
+import com.sendback.domain.user.entity.User;
+import com.sendback.domain.user.service.UserService;
+import com.sendback.global.ServiceTest;
+import com.sendback.global.common.constants.Level;
+import com.sendback.global.config.image.service.ImageService;
+import com.sendback.global.exception.type.BadRequestException;
+import com.sendback.global.exception.type.NotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static com.sendback.domain.feedback.exception.FeedbackExceptionType.DUPLICATE_FEEDBACK_SUBMIT;
+import static com.sendback.domain.feedback.exception.FeedbackExceptionType.NOT_FOUND_FEEDBACK;
+import static com.sendback.domain.feedback.fixture.FeedbackFixture.*;
+import static com.sendback.domain.project.exception.ProjectExceptionType.NOT_PROJECT_AUTHOR;
+import static com.sendback.domain.project.fixture.ProjectFixture.createDummyProject;
+import static com.sendback.domain.user.fixture.UserFixture.createDummyUser;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+public class FeedbackServiceTest extends ServiceTest {
+
+    @InjectMocks
+    FeedbackService feedbackService;
+
+    @Mock
+    UserService userService;
+    @Mock
+    ProjectService projectService;
+    @Mock
+    ImageService imageService;
+    @Mock
+    FeedbackRepository feedbackRepository;
+    @Mock
+    FeedbackSubmitRepository feedbackSubmitRepository;
+
+    private User user = createDummyUser();
+    private Project project = createDummyProject(user);
+    private Feedback feedback = createDummyFeedback(user, project);
+    private final FeedbackSubmit feedbackSubmit = createDummyFeedbackSubmit(user, feedback);
+
+    private final SaveFeedbackRequest saveFeedbackRequest = MOCK_SAVE_FEEDBACK_REQUEST;
+
+    @BeforeEach
+    public void setUp() {
+        this.user = spy(createDummyUser());
+        this.project = spy(createDummyProject(user));
+        this.feedback = spy(createDummyFeedback(user, project));
+    }
+
+    @Nested
+    @DisplayName("피드백 등록 시")
+    class saveFeedback {
+
+        @Test
+        @DisplayName("프로젝트 작성자와 같지 않으면 예외를 발생한다.")
+        public void fail_notAuthor() throws Exception {
+            //given
+            given(userService.getUserById(anyLong())).willReturn(user);
+            given(projectService.getProjectById(anyLong())).willReturn(project);
+            doThrow(new BadRequestException(NOT_PROJECT_AUTHOR)).when(projectService).validateProjectAuthor(any(User.class), any(Project.class));
+
+            //when - then
+            assertThatThrownBy(() -> feedbackService.saveFeedback(1L, 1L, saveFeedbackRequest))
+                    .isInstanceOf(BadRequestException.class)
+                    .hasMessage(NOT_PROJECT_AUTHOR.getMessage());
+        }
+
+        @Test
+        @DisplayName("정상적인 요청일 시 피드백을 저장한다.")
+        public void success() throws Exception {
+            //given
+            given(userService.getUserById(anyLong())).willReturn(user);
+            given(projectService.getProjectById(anyLong())).willReturn(project);
+            doNothing().when(projectService).validateProjectAuthor(any(User.class), any(Project.class));
+            given(feedbackRepository.save(any(Feedback.class))).willReturn(feedback);
+            given(feedback.getId()).willReturn(1L);
+
+            //when
+            FeedbackIdResponse feedbackIdResponse = feedbackService.saveFeedback(1L, 1L, saveFeedbackRequest);
+
+            //then
+            assertThat(feedbackIdResponse.feedbackId()).isEqualTo(1L);
+        }
+    }
+
+    @Nested
+    @DisplayName("피드백 상세 조회 시")
+    class getFeedbackDetail {
+
+        @Test
+        @DisplayName("존재하지 않는 피드백 ID이면 오류를 발생한다.")
+        public void fail_notExistFeedback() throws Exception {
+            //given
+            given(feedbackRepository.findById(anyLong())).willReturn(Optional.empty());
+
+            //when - then
+            assertThatThrownBy(() -> feedbackService.getFeedbackDetail(1L, 1L))
+                    .isInstanceOf(NotFoundException.class)
+                    .hasMessage(NOT_FOUND_FEEDBACK.getMessage());
+
+        }
+
+        @Test
+        @DisplayName("정상적인 요청일 시 값을 반환한다.")
+        public void success() throws Exception {
+            //given
+            given(projectService.getProjectById(anyLong())).willReturn(project);
+            given(feedbackRepository.findById(anyLong())).willReturn(Optional.of(feedback));
+            given(project.getUser()).willReturn(user);
+            given(user.getLevel()).willReturn(Level.ONE);
+            given(feedback.getCreatedAt()).willReturn(LocalDateTime.now());
+
+            //when
+            FeedbackDetailResponse feedbackDetailResponse = feedbackService.getFeedbackDetail(1L, 1L);
+
+            //then
+            assertThat(feedbackDetailResponse.feedbackTitle()).isEqualTo(feedback.getTitle());
+            assertThat(feedbackDetailResponse.username()).isEqualTo(feedback.getUser().getNickname());
+            assertThat(feedbackDetailResponse.projectTitle()).isEqualTo(feedback.getProject().getTitle());
+        }
+    }
+
+    @Nested
+    @DisplayName("피드백을 제출했을 때")
+    class submitFeedback {
+
+        @Test
+        @DisplayName("이미 제출했던 유저면 예외를 발생한다.")
+        public void fail_alreadySubmit() throws Exception {
+            //given
+            given(userService.getUserById(anyLong())).willReturn(user);
+            given(feedbackRepository.findById(anyLong())).willReturn(Optional.of(feedback));
+            given(feedbackSubmitRepository.existsByUserAndFeedbackAndIsDeletedIsFalse(any(User.class), any(Feedback.class))).willReturn(true);
+
+            //when - then
+            assertThatThrownBy(() -> feedbackService.submitFeedback(1L, 1L, mockingMultipartFile("screenShot")))
+                    .isInstanceOf(BadRequestException.class)
+                    .hasMessage(DUPLICATE_FEEDBACK_SUBMIT.getMessage());
+        }
+
+        @Test
+        @DisplayName("레벨업이 되지 않는 상태면 유저 레벨을 유지하고 값을 반환한다.")
+        public void success_keep() throws Exception {
+            //given
+            given(userService.getUserById(anyLong())).willReturn(user);
+            given(feedbackRepository.findById(anyLong())).willReturn(Optional.of(feedback));
+            given(feedbackSubmitRepository.existsByUserAndFeedbackAndIsDeletedIsFalse(any(User.class), any(Feedback.class))).willReturn(false);
+            given(imageService.uploadImage(any(), anyString())).willReturn("sendback1.jpg");
+            given(feedbackSubmitRepository.save(any(FeedbackSubmit.class))).willReturn(feedbackSubmit);
+            given(feedbackSubmitRepository.countByUserAndIsDeletedIsFalse(any(User.class))).willReturn(1L);
+            given(user.getLevel()).willReturn(Level.ONE);
+
+            //when
+            SubmitFeedbackResponse response = feedbackService.submitFeedback(1L, 1L, mockingMultipartFile("feedback"));
+
+            //then
+            assertThat(response.level()).isEqualTo(user.getLevel().getName());
+            assertThat(response.isLevelUp()).isFalse();
+            assertThat(response.remainFeedbackCount()).isEqualTo(4L);
+        }
+
+        @Test
+        @DisplayName("레벨업이 되지 않는 상태면 유저 레벨을 유지하고 값을 반환한다.")
+        public void success_levelUp() throws Exception {
+            //given
+            given(userService.getUserById(anyLong())).willReturn(user);
+            given(feedbackRepository.findById(anyLong())).willReturn(Optional.of(feedback));
+            given(feedbackSubmitRepository.existsByUserAndFeedbackAndIsDeletedIsFalse(any(User.class), any(Feedback.class))).willReturn(false);
+            given(imageService.uploadImage(any(), anyString())).willReturn("sendback1.jpg");
+            given(feedbackSubmitRepository.save(any(FeedbackSubmit.class))).willReturn(feedbackSubmit);
+            given(feedbackSubmitRepository.countByUserAndIsDeletedIsFalse(any(User.class))).willReturn(5L);
+            given(user.getLevel()).willReturn(Level.ONE).willReturn(Level.TWO);
+
+            //when
+            SubmitFeedbackResponse response = feedbackService.submitFeedback(1L, 1L, mockingMultipartFile("feedback"));
+
+            //then
+            assertThat(response.level()).isEqualTo(Level.TWO.getName());
+            assertThat(response.isLevelUp()).isTrue();
+            assertThat(response.remainFeedbackCount()).isEqualTo(5L);
+        }
+    }
+
+}
