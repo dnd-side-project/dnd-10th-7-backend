4a6edfe1f1950c66a788a97f3a5b1ecd6d32a74c
WooYoungDoo <duden5000@naver.com>
2024-02-14 20:06:01 +0900

    test : mockFile 매서드 최적화

--- src/test/java/com/sendback/domain/project/service/ProjectServiceTest.java
+++ src/test/java/com/sendback/domain/project/service/ProjectServiceTest.java
@@ -14,26 +14,18 @@ import com.sendback.domain.user.service.UserService;
 import com.sendback.global.ServiceTest;
 import com.sendback.global.config.image.service.ImageService;
 import com.sendback.global.exception.type.BadRequestException;
-import com.sendback.global.exception.type.ImageException;
 import com.sendback.global.exception.type.NotFoundException;
 import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 import static com.sendback.domain.project.exception.ProjectExceptionType.*;
 import static com.sendback.domain.project.fixture.ProjectFixture.*;
 import static com.sendback.domain.user.fixture.UserFixture.createDummyUser;
-import static com.sendback.global.config.image.exception.ImageExceptionType.AWS_S3_UPLOAD_FAIL;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.ArgumentMatchers.any;
@@ -224,26 +216,6 @@ public class ProjectServiceTest extends ServiceTest {
 
             //when - then
             assertDoesNotThrow(() -> projectService.deleteProject(1L, 1L));
-        }
-    }
-
-    private MultipartFile mockingMultipartFile(String fileName) {
-        return new MockMultipartFile(
-                "images",
-                fileName,
-                MediaType.IMAGE_JPEG_VALUE,
-                generateMockImage()
-        );
-    }
-
-    private byte[] generateMockImage() {
-        BufferedImage image = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
-
-        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
-            ImageIO.write(image, "jpg", byteArrayOutputStream);
-            return byteArrayOutputStream.toByteArray();
-        } catch (IOException e) {
-            throw new ImageException(AWS_S3_UPLOAD_FAIL);
         }
     }
 
