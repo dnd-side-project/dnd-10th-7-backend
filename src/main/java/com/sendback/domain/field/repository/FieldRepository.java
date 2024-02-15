f15c582aa103ed7b1cb12e4313f2bb8d5d68c55e
WooYoungDoo <duden5000@naver.com>
2024-02-15 20:19:36 +0900

    refactor : findByName 삭제

--- src/main/java/com/sendback/domain/field/repository/FieldRepository.java
+++ src/main/java/com/sendback/domain/field/repository/FieldRepository.java
@@ -3,9 +3,5 @@ package com.sendback.domain.field.repository;
 import com.sendback.domain.field.entity.Field;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface FieldRepository extends JpaRepository<Field, Long> {
-
-    Optional<Field> findByName(String name);
 }
