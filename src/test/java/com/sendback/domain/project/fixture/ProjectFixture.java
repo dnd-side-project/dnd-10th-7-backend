1502eb503af3c4e3deed7e3a9c4df09afa4226fc
WooYoungDoo <duden5000@naver.com>
2024-02-16 13:41:03 +0900

    test : 프로젝트 상세 조회 api response 추가 #23

--- src/test/java/com/sendback/domain/project/fixture/ProjectFixture.java
+++ src/test/java/com/sendback/domain/project/fixture/ProjectFixture.java
@@ -2,13 +2,18 @@ package com.sendback.domain.project.fixture;
 
 import com.sendback.domain.project.dto.request.SaveProjectRequestDto;
 import com.sendback.domain.project.dto.request.UpdateProjectRequestDto;
+import com.sendback.domain.project.dto.response.ProjectDetailResponseDto;
 import com.sendback.domain.project.entity.Progress;
 import com.sendback.domain.project.entity.Project;
 import com.sendback.domain.user.entity.User;
 import com.sendback.global.common.constants.FieldName;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+
+import static com.sendback.domain.user.fixture.UserFixture.mock_user;
+import static com.sendback.global.util.CustomDateUtil.customDateFormat;
 
 public class ProjectFixture {
 
@@ -36,6 +41,12 @@ public class ProjectFixture {
             UPDATE_TITLE, UPDATE_FIELD_NAME.getName(), UPDATE_CONTENT, UPDATE_SUMMARY, UPDATE_DEMO_SITE_URL, UPDATE_START_DATE, UPDATE_END_DATE, DEVELOPING_PROGRESS.getValue(),
             4L, 3L, 2L, 1L, List.of("deleteUrl")
     );
+
+    public static final ProjectDetailResponseDto MOCK_PROJECT_DETAIL_RESPONSE_DTO = new ProjectDetailResponseDto(
+            1L, mock_user.getNickname(), mock_user.getLevel().getName(), mock_user.getProfileImageUrl(), 1L, TITLE, FIELD_NAME.getName(),
+            CONTENT, DEMO_SITE_URL, PLANNING_PROGRESS.getValue(), List.of("이미지 1", "이미지 2"), 1L, 2L, 3L, 4L, 5L, 6L, 7L,
+            customDateFormat(LocalDateTime.now()), LocalDate.of(2024, 1, 12).toString(), LocalDate.of(2024, 1, 15).toString(),
+            false, false, false);
 
     public static Project createDummyProject(User user) {
         return Project.of(user, MOCK_SAVE_PROJECT_REQUEST_DTO);
