7c9b84ab4266bdf4b34b55ff25070d75cb259d34
WooYoungDoo <duden5000@naver.com>
2024-02-17 01:19:42 +0900

    test : mock feedbacks response dto #23

--- src/test/java/com/sendback/domain/feedback/fixture/FeedbackFixture.java
+++ src/test/java/com/sendback/domain/feedback/fixture/FeedbackFixture.java
@@ -2,6 +2,8 @@ package com.sendback.domain.feedback.fixture;
 
 import com.sendback.domain.feedback.dto.request.SaveFeedbackRequestDto;
 import com.sendback.domain.feedback.dto.response.FeedbackDetailResponseDto;
+import com.sendback.domain.feedback.dto.response.FeedbackResponse;
+import com.sendback.domain.feedback.dto.response.GetFeedbacksResponse;
 import com.sendback.domain.feedback.dto.response.SubmitFeedbackResponseDto;
 import com.sendback.domain.feedback.entity.Feedback;
 import com.sendback.domain.feedback.entity.FeedbackSubmit;
@@ -10,6 +12,10 @@ import com.sendback.domain.user.entity.User;
 import com.sendback.global.common.constants.Level;
 
 import java.time.LocalDate;
+import java.util.List;
+
+import static com.sendback.domain.project.entity.Progress.PLANNING;
+import static com.sendback.global.common.constants.FieldName.ART;
 
 public class FeedbackFixture {
 
@@ -27,10 +33,26 @@ public class FeedbackFixture {
     public static final FeedbackDetailResponseDto MOCK_FEEDBACK_DETAIL_RESPONSE = new FeedbackDetailResponseDto(
             1L, "유저 닉네임", Level.ONE.getName(), "프로필 이미지 url", 1L, "피드백 제목", "피드백 링크",
             "피드백 내용", "추가 리워드 메시지", "yyyy-MM-dd HH:mm", LocalDate.of(2024, 1, 12).toString(),
-            LocalDate.of(2024, 1, 15).toString(),1L, "프로젝트 ID", "art", "PLANNING");
+            LocalDate.of(2024, 1, 15).toString(),1L, "프로젝트 ID", ART.getName(), PLANNING.getValue());
 
     public static final SubmitFeedbackResponseDto MOCK_SUBMIT_FEEDBACK_RESPONSE = new SubmitFeedbackResponseDto(
             Level.ONE.getName(), false, 4L);
+
+    public static final FeedbackResponse MOCK_FEEDBACK_RESPONSE_A = new FeedbackResponse(
+            1L, "기획 피드백 부탁해요", "아메리카노 5개",
+            LocalDate.of(2024, 1, 12).toString(),
+            LocalDate.of(2024, 1, 15).toString(), false, false, false
+    );
+
+    public static final FeedbackResponse MOCK_FEEDBACK_RESPONSE_B = new FeedbackResponse(
+            2L, "와이어 프레임 피드백 부탁해요", "빙수 5개",
+            LocalDate.of(2024, 1, 15).toString(),
+            LocalDate.of(2024, 1, 18).toString(), true, true, false
+    );
+
+    public static final GetFeedbacksResponse MOCK_GET_FEEDBACK_RESPONSE = new GetFeedbacksResponse(
+            List.of(MOCK_FEEDBACK_RESPONSE_A, MOCK_FEEDBACK_RESPONSE_B)
+    );
 
     public static FeedbackSubmit createDummyFeedbackSubmit(User user, Feedback feedback) {
         return FeedbackSubmit.of(user, feedback, SCREEN_SHOT_URL);
