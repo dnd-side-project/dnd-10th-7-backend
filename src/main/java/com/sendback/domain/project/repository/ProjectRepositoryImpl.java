133fd60c3bb5742206d82534c47f5f7f5cfa4ed3
DESKTOP-71386SR\hal8h <strangehoon2@gmail.com>
2024-02-16 18:48:42 +0900

    feat : 내가 등록한 프로젝트 페이징 querydsl로 구현 #27

---
+++ src/main/java/com/sendback/domain/project/repository/ProjectRepositoryImpl.java
@@ -0,0 +1,51 @@
+package com.sendback.domain.project.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sendback.domain.project.entity.QProject;
+import com.sendback.domain.user.dto.response.QRegisteredProjectResponseDto;
+import com.sendback.domain.user.dto.response.RegisteredProjectResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+import static com.sendback.domain.project.entity.QProject.project;
+
+@RequiredArgsConstructor
+public class ProjectRepositoryImpl implements ProjectRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Page<RegisteredProjectResponseDto> findAllProjectsByMe(Pageable pageable, Long userId, Boolean check) {
+
+        List<RegisteredProjectResponseDto> content = queryFactory
+                .select(new QRegisteredProjectResponseDto(
+                       project.id.as("projectId"),
+                       project.title,
+                       project.progress.stringValue(),
+                       project.summary,
+                       project.createdAt,
+                       project.projectPull.pullUpCnt
+                ))
+                .from(project)
+                .where(project.user.id.eq(userId),
+                        project.isFinished.eq(check),
+                        project.isDeleted.eq(false))
+                .orderBy(project.createdAt.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        int total = queryFactory
+                .selectFrom(project)
+                .where(project.user.id.eq(userId),
+                        project.isFinished.eq(check),
+                        project.isDeleted.eq(false))
+                .fetch().size();
+
+        return new PageImpl<>(content, pageable, total);
+    }
+}
