60b841ccf05fad3894c5995083083dea9444e9b4
DESKTOP-71386SR\hal8h <strangehoon2@gmail.com>
2024-02-18 19:12:32 +0900

    feat : findRecommendedProjects 메서드 구현 #33

--- src/main/java/com/sendback/domain/project/repository/ProjectRepositoryImpl.java
+++ src/main/java/com/sendback/domain/project/repository/ProjectRepositoryImpl.java
@@ -1,13 +1,19 @@
 package com.sendback.domain.project.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sendback.domain.project.dto.response.QRecommendedProjectResponseDto;
+import com.sendback.domain.project.dto.response.RecommendedProjectResponseDto;
 import com.sendback.domain.user.dto.response.QRegisteredProjectResponseDto;
 import com.sendback.domain.user.dto.response.RegisteredProjectResponseDto;
+import com.sendback.global.common.constants.FieldName;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import java.util.List;
+
+import static com.sendback.domain.like.entity.QLike.like;
 import static com.sendback.domain.project.entity.QProject.project;
 
 @RequiredArgsConstructor
@@ -45,5 +51,36 @@ public class ProjectRepositoryImpl implements ProjectRepositoryCustom {
                 .fetch().size();
 
         return new PageImpl<>(content, pageable, total);
+    }
+
+    @Override
+    public List<RecommendedProjectResponseDto> findRecommendedProjects(Long userId, List<FieldName> filedNameList) {
+        List<RecommendedProjectResponseDto> content = queryFactory
+                .select(new QRecommendedProjectResponseDto(
+                        project.id.as("projectId"),
+                        project.progress.stringValue(),
+                        project.fieldName.stringValue(),
+                        project.title,
+                        project.summary,
+                        project.user.nickname,
+                        project.createdAt,
+                        project.user.profileImageUrl
+                ))
+                .from(project)
+                .join(project.likes, like)
+                .groupBy(project)
+                .where(
+                        like.isDeleted.eq(false),
+                        project.isDeleted.eq(false),
+                        userIdIs(userId, filedNameList)
+                )
+                .orderBy(like.count().desc())
+                .limit(10)
+                .fetch();
+        return content;
+    }
+
+    private BooleanExpression userIdIs(Long userId, List<FieldName> fileNameList) {
+        return userId == null ?  null : project.fieldName.in(fileNameList);
     }
 }
