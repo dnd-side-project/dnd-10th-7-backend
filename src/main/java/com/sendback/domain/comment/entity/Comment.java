3ada08c151bfea207c5d58800bbd4534b9ec4767
DESKTOP-71386SR\hal8h <strangehoon2@gmail.com>
2024-02-19 05:32:41 +0900

    feat : comment 엔티티 설정 #38

--- src/main/java/com/sendback/domain/comment/entity/Comment.java
+++ src/main/java/com/sendback/domain/comment/entity/Comment.java
@@ -1,6 +1,7 @@
 package com.sendback.domain.comment.entity;
 
 import com.sendback.domain.project.entity.Project;
+import com.sendback.domain.user.entity.User;
 import com.sendback.global.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -17,6 +18,17 @@ public class Comment extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String content;
+
+    private Long ref;
+    private Long step;
+    private Long refOrder;
+    private Long answerNum;
+    private Long parentNum;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="user_id")
+    private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "project_id")
