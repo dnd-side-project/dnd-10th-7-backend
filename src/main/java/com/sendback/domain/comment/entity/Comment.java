cd7bfbf09f678a89ddbc9219b3e3e76f8613fa84
DESKTOP-71386SR\hal8h <strangehoon2@gmail.com>
2024-02-20 01:20:54 +0900

    refactor : Comment 엔티티 리팩토링 #38

--- src/main/java/com/sendback/domain/comment/entity/Comment.java
+++ src/main/java/com/sendback/domain/comment/entity/Comment.java
@@ -1,10 +1,11 @@
 package com.sendback.domain.comment.entity;
 
 import com.sendback.domain.project.entity.Project;
-import com.sendback.domain.user.entity.User;
+import com.sendback.domain.user.entity.*;
 import com.sendback.global.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
@@ -14,18 +15,10 @@ import org.hibernate.annotations.SQLDelete;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @SQLDelete(sql = "UPDATE comment SET is_deleted = true WHERE id = ?")
 public class Comment extends BaseEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
-
-    private Long ref;
-    private Long step;
-    private Long refOrder;
-    private Long answerNum;
-    private Long parentNum;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name ="user_id")
     private User user;
@@ -35,4 +28,19 @@ public class Comment extends BaseEntity {
     private Project project;
 
     private boolean isDeleted = false;
+
+    @Builder
+    public Comment(Long id, String content, User user, Project project ) {
+        this.content = content;
+        this.user = user;
+        this.project = project;
+    }
+
+    public static Comment of(String content, User user, Project project){
+        return Comment.builder()
+                .content(content)
+                .user(user)
+                .project(project)
+                .build();
+    }
 }
