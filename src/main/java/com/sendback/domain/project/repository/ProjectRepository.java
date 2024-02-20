6aae6f51e3b486acdb7ed78ad7fa280f85bbf13d
WooYoungDoo <duden5000@naver.com>
2024-02-20 14:49:55 +0900

    feat : 종료일이 지났는지 조회 #40

--- src/main/java/com/sendback/domain/project/repository/ProjectRepository.java
+++ src/main/java/com/sendback/domain/project/repository/ProjectRepository.java
@@ -4,9 +4,11 @@ import com.sendback.domain.project.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, Long>, ProjectRepositoryCustom {
     Long countByUserId(Long userId);
     List<Project> findByUserId(Long userId);
+    List<Project> findAllByEndedAtBeforeAndIsDeletedIsFalse(LocalDate localDate);
 }
