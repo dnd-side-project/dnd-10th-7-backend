654d2600c2ec2fafa1d4a2f305de0857e0dce6f2
WooYoungDoo <duden5000@naver.com>
2024-02-20 14:50:52 +0900

    feat : pullUp을 사용한 사용자 조회 #40

--- src/main/java/com/sendback/domain/user/repository/UserRepository.java
+++ src/main/java/com/sendback/domain/user/repository/UserRepository.java
@@ -4,9 +4,11 @@ import com.sendback.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findBySocialId(String socialId);
     Optional<User> findByNickname(String nickname);
+    List<User> findAllByPullUpCntIsGreaterThan(Long cnt);
 }
