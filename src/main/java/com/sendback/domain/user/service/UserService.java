45219106c961640edb7dbbbeba515373cfa5228f
DESKTOP-71386SR\hal8h <strangehoon2@gmail.com>
2024-02-22 18:17:26 +0900

    feat : 중복 회원 가입 불가 로직 추가 #45

--- src/main/java/com/sendback/domain/user/service/UserService.java
+++ src/main/java/com/sendback/domain/user/service/UserService.java
@@ -53,6 +53,9 @@ public class UserService {
         }
         jwtProvider.validateSignToken(signUpRequestDto.signToken());
         SigningAccount signingAccount = jwtProvider.getSignUserInfo(signUpRequestDto.signToken());
+        if(userRepository.findBySocialId(signingAccount.socialId()).isPresent()){
+            throw new BadRequestException(PREVIOUS_REGISTERED_USER);
+        }
         User user = User.of(signingAccount, signUpRequestDto);
         User savedUser = userRepository.save(user);
         List<Field> fieldList = signUpRequestDto.fields().stream()
